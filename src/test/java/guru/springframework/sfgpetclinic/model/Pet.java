1709a25d938da2964a32bc64bf3a1628d1702bc1
sbao23 <simon.bao@rvaonem.fgov.be>
2023-01-20 16:01:06 +0100

    adding POJOS. closes #2

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,35 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+import java.util.Locale;
+
+public class Pet {
+
+    private Owner owner;
+    private PetType petType;
+    private LocalDate brithday;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public LocalDate getBrithday() {
+        return brithday;
+    }
+
+    public void setBrithday(LocalDate brithday) {
+        this.brithday = brithday;
+    }
+}
