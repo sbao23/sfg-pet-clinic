89e6fb47842fb5214a27e2fa09af03fc8bb90d9c
sbao23 <simon.bao@rvaonem.fgov.be>
2023-01-23 14:15:06 +0100

    adding base entity closes #9

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private Owner owner;
     private PetType petType;
