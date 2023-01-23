febb27668c0cfb139a0f54a15f38f26d609385ed
sbao23 <simon.bao@rvaonem.fgov.be>
2023-01-23 15:15:38 +0100

    adding common interface+refactor services closes #10

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -4,9 +4,6 @@ import guru.springframework.sfgpetclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet,Long>{
 
 }
