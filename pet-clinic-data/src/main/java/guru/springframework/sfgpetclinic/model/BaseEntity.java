32dacec565e616117aeb1bc0546cbc33e56393e7
sbao23 <simon.bao@rvaonem.fgov.be>
2023-01-23 15:38:40 +0100

    adding map services closes #5

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class BaseEntity implements Serializable {
     private Long id;
@@ -11,5 +12,20 @@ public class BaseEntity implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BaseEntity that = (BaseEntity) o;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
