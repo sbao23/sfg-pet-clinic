632ddbe98becebb7efa0b3b9e53173b94a9abbf4
sbao23 <simon.bao@rvaonem.fgov.be>
2023-01-23 14:06:41 +0100

    Add interface for pet Clinic service closes #4

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public interface OwnerService {
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
